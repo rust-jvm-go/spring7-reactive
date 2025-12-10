e41f6c4244b8ae70e57ca3c1b852298ca651e0ff
rust-jvm-go <rustxjvmxgo@gmail.com>
2025-12-10 12:46:50 +0800

    Initial project scaffold

---
+++ src/main/java/initiative/java/spring7plus/spring7reactive/Spring7ReactiveApplication.java
@@ -0,0 +1,13 @@
+package initiative.java.spring7plus.spring7reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring7ReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring7ReactiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/initiative/java/spring7plus/spring7reactive/Spring7ReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package initiative.java.spring7plus.spring7reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring7ReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
